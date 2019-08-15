a20eeeaeb95537406ae38bc6efd8e23d8d268422
Jose Carlos de Jesus Junior <jjunior@edfmancapital.com>
2019-08-15 15:01:27 -0300

    Navigation and Routing Additional Techniques

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../Interfaces/IProduct';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({templateUrl: './product-detail.component.html',styleUrls: ['./product-detail.component.css']})
 export class ProductDetailComponent implements OnInit
@@ -7,13 +8,36 @@ export class ProductDetailComponent implements OnInit
   pageTitle:string = 'Product Detail';
   product: IProduct;
 
-  constructor() 
+  // Constructor
+  constructor(private route: ActivatedRoute, private router: Router) 
   { 
 
   }
 
   ngOnInit()
   {
+    let id = +this.route.snapshot.paramMap.get('id');
+
+    console.log(id);
+
+    this.pageTitle += ` ${id}`;
+
+    this.product =  
+    {
+      "productId": id,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "http://96-87-153-66-static.hfc.comcastbusiness.net/cliparts/7/c/5/a/clipart-leaf-rake-256x256-7c5a.png"
+    };
+  }
+
+  onBack(): void
+  {
+    this.router.navigate(['/products']);
   }
 
 }
