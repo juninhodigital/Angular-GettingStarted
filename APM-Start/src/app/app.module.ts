02646a94b1bc6f42950fe9c7a6d8dae16c682fe7
Jose Carlos de Jesus Junior <jjunior@edfmancapital.com>
2019-08-15 11:42:53 -0300

    Navigation and Routing Basics

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -8,6 +9,8 @@ import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacePipe } from './Pipes/convert-to-space.pipe';
 import { StarComponent } from './shared/star.component';
 import { ProductService } from './services/product.service';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: 
@@ -15,14 +18,24 @@ import { ProductService } from './services/product.service';
     AppComponent,
     ProductListComponent,
     ConvertToSpacePipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
     // ProductService
   ],
   imports: 
   [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot
+    ([
+      { path: 'products', component: ProductListComponent},
+      { path: 'products/:id', component: ProductDetailComponent},
+      { path: 'welcome', component: WelcomeComponent},
+      { path: '', redirectTo:'welcome', pathMatch: 'full'},
+      { path: '**', redirectTo:'welcome', pathMatch: 'full'},
+    ])
   ],
   bootstrap: [AppComponent]
 })
